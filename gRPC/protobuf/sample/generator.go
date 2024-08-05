c6d5e53f814aa0c6cf950bc26083c23d0c6eb5dc
jopaleti <oopaletijohnson@gmail.com>
2024-08-05 23:53:05 +0100

    ...

--- gRPC/protobuf/sample/generator.go
+++ gRPC/protobuf/sample/generator.go
@@ -15,8 +15,16 @@ func NewKeyBoard() *pb.Keyboard {
 
 func NewCPU() *pb.CPU {
 	brand := randomCPUBrand()
-	cpu := &pb.CPU{
+	name := randomCPUName(brand)
+	numberCores := randomCores(2, 8)
+	numberThreads := randomInt(numberCores, 32)
+	minGhz := randomFloat64(2.0, 3.5)
 
+	cpu := &pb.CPU{
+		Brand: brand,
+		Name: name,
+		NumberCores: uint32(numberCores),
+		NumberThreads: uint32(numberThreads),
 	}
 	return cpu
 }
--- gRPC/protobuf/sample/random.go
+++ gRPC/protobuf/sample/random.go
@@ -22,13 +22,51 @@ func randomBool() bool {
 }
 
 func randomCPUBrand() string {
-	return randomStringFromSet("Intel", "AMD", "MACOS")
+	return randomStringFromSet("Intel", "MACOS")
 }
 
+func randomCPUName(brand string) string {
+	if brand == "Intel" {
+		return randomStringFromSet(
+			"core i3",
+			"core i4",
+			"core i5",
+			"core i6",
+			"core i7",
+			"core i9",
+		)
+	}
+
+	return randomStringFromSet(
+		"MAC core i5",
+		"MAC core i7",
+		"MAC core i9",
+		"MAC M1",
+		"MAC M1 Pro",
+		"MAC M2",
+		"MAC M2 Pro",
+		"MAC M3",
+		"MAC M3 Pro",
+	)
+}
+
+// RandomStringFromSet
 func randomStringFromSet(s ...string) string {
 	n := len(s) 
 	if n == 0 {
 		return ""
 	}
 	return s[rand.Intn(n)]
+}
+
+func randomCores(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min+1)
+}
+
+func randomFloat64(min, max float64) float64 {
+	return min + rand.Float64()
 }
